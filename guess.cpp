6a3d6f93248a0c6251a4c636515a553b912d7005
YvesHarrsion <yveslee15@gmail.com>
2015-03-27 22:44:31 +0800

    Revert "numberguessing"
    This reverts commit 9df2699a13ab13f4f06d618fdd83a72c007768d8.

--- guess.cpp
+++ guess.cpp
@@ -34,7 +34,7 @@ int main()
 			rightposition=0;
 			rightnumber=0;
 			
-			while(t<7){
+			while(t<8){
 				
 				cout<<"enter number1"<<endl;
 				cin>>input1;
@@ -73,12 +73,12 @@ int main()
 					}
 				else {
 					cout<<rightposition<<"A"<<rightnumber<<"B"<<endl;
+					++t;
 					rightposition=0;
 					rightnumber=0;
-					++t;
 					}}
 			
-			if(t=7){
+			if(t=8){
 				++lose;
 				cout<<"win"<<win<<"lose"<<lose<<endl;
 				cout<<"sorry you lose.do you want play again?"<<endl;
